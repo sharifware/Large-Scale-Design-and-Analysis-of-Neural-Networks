5d217a001b9547a7ba34e299c24e82bafbe6a694
sharifware <sharifadepetu@gmail.com>
2025-02-20 19:51:01 -0500

    fixed bug and corrected upload

--- nnanalyzer/setup.py
+++ nnanalyzer/setup.py
@@ -5,7 +5,7 @@ with open("Readme.md","r") as f:
 
 setup(
     name="nnanalyzer",
-    version="0.1.0",
+    version="0.1.1",
     description="A series of tools created to analyze neural networks ",
     package_dir={"":"app"},
     packages=find_packages(where="app"),
@@ -13,7 +13,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/sharifware/Large-Scale-Design-and-Analysis-of-Neural-Networks",
     author="ERAU Large Scale Design & Analysis of Neural Networks",
-    install_requires=["matplotlib", "importlib", "torch", "pandas", "numpy", "scikit-learn", "regex",
+    install_requires=["matplotlib", "torch", "pandas", "numpy", "scikit-learn", "regex",
                       "scipy"],
     python_requires=">3.8",
 )
